9aefc82909f322778def9cb05dde23892396b160
Christopher Lee <lee@ashimaresearch.com>
2012-06-02 15:57:21 -0700

    Adding analemma example to docs

---
+++ docs/examples.rst
@@ -0,0 +1,29 @@
+========================
+Examples
+========================
+
+1. Plotting the Mars Analemma 
+---
+
+The Mars Anelemma shows the angular offset of the Sun from its mean position throughout the year. We can plot this by plotting the equation of time and the solar declination.
+
+.. code-block :: python
+
+	import Mars24
+	#define the start date as sometime in May 2000, start of MY25
+	start_j2000_ott = 151.27365 
+
+	#create the calendar array with 120 points
+	msd = np.linspace(0,669,120)
+	
+	#calculate the j2000 offset dates
+	j2000_offsets = Mars24.j2000_from_Mars_Solar_Date(msd + Mars24.Mars_Solar_Date(start_j2000_ott))
+
+	#calculate the equation of time in minutes and the declination.
+	eot = Mars24.equation_of_time(j2000_offsets)*60/15. #convert from degrees to minutes
+	dec = Mars24.solar_declination(Mars24.Mars_Ls(j2000_offsets)) #takes Ls, not MSD
+	
+Plotting ``eot`` against ``dec`` gives the following plot ('months' are also marked in red).
+
+.. figure:: analemma.png
+	:align: center
---
+++ docs/plot_analemma.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import matplotlib as mpl
+mpl.use("Agg")
+mpl.rc("axes",labelsize=10)
+mpl.rc("xtick",labelsize=10)
+mpl.rc("ytick",labelsize=10)
+mpl.rc("legend",fontsize=10)
+import pylab as pl
+import numpy as np
+import Mars24
+
+#define mars solar days to use in the plot
+start_j2000_ott = 151.27365 # sometime in May 2000, start of MY25
+
+msd = np.linspace(0,669,120)
+#calculate the j2000 offset dates
+j2000_offsets = Mars24.j2000_from_Mars_Solar_Date(msd + Mars24.Mars_Solar_Date(start_j2000_ott))
+
+eot_axis = Mars24.equation_of_time(j2000_offsets)*60/15. #convert from degrees to minutes
+dec_axis = Mars24.solar_declination(Mars24.Mars_Ls(j2000_offsets)) #takes Ls, not MSD
+
+pl.figure(figsize=(4,6))
+pl.subplots_adjust(left=0.15)
+pl.plot(eot_axis, dec_axis, marker='.', ls='none',color='k')
+pl.plot(eot_axis[::10], dec_axis[::10], marker='.', ls='none',color='r')
+pl.ylim(-28,28)
+pl.xlim(-55,45)
+pl.xticks(np.arange(-50,50,10))
+pl.yticks(np.arange(-30,30,5))
+pl.ylabel("Declination (degrees)")
+pl.xlabel("Equation of Time (minutes)")
+pl.savefig("analemma.png")
